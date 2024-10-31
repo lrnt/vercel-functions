b89d047197815420b65cce3e0133f7221d79faac
Laurent De Marez <laurent@demarez.org>
2024-10-31 17:31:49 +0100

    nok

--- api/hello.ts
+++ api/hello.ts
@@ -2,6 +2,6 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   return res.json({
-    message: "ok",
+    message: "nok",
   });
 }
